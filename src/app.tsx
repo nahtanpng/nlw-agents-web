0806d6c0c43bb78a151cf8b59ab79fefc58f944a
nahtanpng <nathan.ferreiira.dev@gmail.com>
2025-07-09 15:34:11 -0300

    feat: add routes and rooms query

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,19 @@
-import { Button } from './components/ui/button';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { CreateRoom } from './pages/create-room';
+import { Room } from './pages/room';
+
+const queryClient = new QueryClient();
 
 export function App() {
   return (
-    <div>
-      <Button>Hello World</Button>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<CreateRoom />} index />
+          <Route element={<Room />} path="/room/:roomId" />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
